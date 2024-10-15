9dca666244c820958d9c9b296867a9bc3a734f25
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-10-16 01:11:20 +0600

    master and home page done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyCommerceController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MyCommerceController::class,'index'])->name('home');
+Route::get('/product-categorey', [MyCommerceController::class,'category'])->name('product-categorey');
+Route::get('/product-detail', [MyCommerceController::class,'detail'])->name('product-detail');
